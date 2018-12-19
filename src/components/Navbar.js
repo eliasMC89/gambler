cdcda3ac0961146114e9c5c1c570a51203f17141
Elias Miralles <elias.miralles@gmail.com>
2018-12-19 21:46:06 +0100

    add menu list navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,56 @@ import { withAuth } from '../providers/AuthProvider';
 
 class Navbar extends Component {
 
+  state = {
+    isShowMenu: false,
+  }
+
+  handleClickMenu = () => {
+    const { isShowMenu } = this.state;
+    if (isShowMenu) {
+      this.setState({
+        isShowMenu: false,
+      })
+    } else {
+      this.setState({
+        isShowMenu: true,
+      })
+    }
+  }
+
+  notShowMenu = () => {
+    return(
+      <div>
+        <button onClick={this.handleClickMenu} className="menu-btn"><img src={require('./menu1.png')} alt="menu" className="menu-img"/></button>
+      </div>
+    )
+  }
+
+  showMenu = () => {
+    return (
+      <div className="menu-box">
+        <div className="menu-btn">
+          <button onClick={this.handleClickMenu} className="hide-menu-btn">Hide</button>
+        </div>
+        <ul className="nav-menu">
+          <li>My info</li>
+          <li>Odds calculator</li>
+          <li>My games</li>
+        </ul>
+      </div>
+    )
+  }
+
   renderIsLoggedIn = () => {
     return (
     <div className="navbar">
       <div className="home-link">
         <Link to="/home" className="nav-link"><img src={require('./trebol-poker.png')} alt="logo" className="navbar-logo"/></Link>
       </div>
-      <div className="profile-link">
-        {/* <p>Hi, <Link to="/profile/my-info" ></Link></p> */}
+      {/* <div className="profile-link">
         <Link to="/profile/my-info" className="nav-link">{this.props.user.username}</Link>
-      </div>
+      </div> */}
+      { this.state.isShowMenu ? this.showMenu() : this.notShowMenu()}
     </div>
     )
   }
