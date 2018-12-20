c7c54f8e58af1a4be7dafc4aa70cb3e114f99497
Elias Miralles <elias.miralles@gmail.com>
2018-12-20 10:18:23 +0100

    button transparent mobile

--- src/components/SummaryPlayerCard.js
+++ src/components/SummaryPlayerCard.js
@@ -31,25 +31,3 @@ class SummaryPlayerCard extends Component {
 }
 
 export default SummaryPlayerCard;
-{/* <li className="current-player-card">
-  <div className="current-player-info"><span className="card-name-tag">{player.name}</span></div>
-  <div className="summary-player-info bi-fs-infobox">
-    <p>Total buy in: {player.buyin} $</p>
-    <p>Final Stack: {player.finalStack} $</p>
-  </div>
-  <div className="buyins-box">
-    <div className="summary-player-info">
-      <p>Buyins:</p>
-    </div >
-    <ul className="buyin-history">
-      {player.buyinHistory.map((buyin, index)=>{
-        return (
-          <li key={`id=${index}`} className="buyin-list"> + {buyin}</li>
-        )
-      })}
-    </ul>
-  </div>
-  <div className="summary-player-win">
-    <p>Wins: {player.finalStack - player.buyin}</p>
-  </div>
-</li> */}
